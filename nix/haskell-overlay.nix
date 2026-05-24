3d0700821148f7d1c4f00c0ff89fe2ea18c75837
Nadeem Bitar <nadeem@gmail.com>
2026-05-24 07:57:32 -0700

    refactor(nix): use shared baikai overlays

--- nix/haskell-overlay.nix
+++ nix/haskell-overlay.nix
@@ -1,50 +1,11 @@
 { pkgs
 , gitRev
 , seihou-schema-src ? null
-, baikai-src ? null
-, claude-src ? null
-, openai-src ? null
-, cradle-src ? null
 }:
 let
   inherit (pkgs.haskell.lib.compose) doJailbreak;
-  stageBaikaiRootFiles = drv: {
-    prePatch = (drv.prePatch or "") + ''
-      cp ${baikai-src}/CHANGELOG.md ../CHANGELOG.md
-      cp ${baikai-src}/LICENSE ../LICENSE
-    '';
-  };
 in
 final: prev: {
-  wai-app-static = pkgs.haskell.lib.compose.dontCheck (doJailbreak (final.callHackageDirect
-    {
-      pkg = "wai-app-static";
-      ver = "3.1.9.1";
-      sha256 = "1irlknakxl7dcwxxdw0iliql7xrbyssz4bdk18amr2xl2d0fcwzc";
-    }
-    { }));
-
-  cradle = pkgs.haskell.lib.compose.dontCheck (doJailbreak (final.callCabal2nix "cradle" cradle-src { }));
-
-  claude = pkgs.haskell.lib.compose.dontCheck (doJailbreak (final.callCabal2nix "claude" (claude-src + "/claude") { }));
-
-  openai = pkgs.haskell.lib.compose.dontCheck (doJailbreak (final.callCabal2nix "openai" (openai-src + "/openai") { }));
-
-  baikai = pkgs.haskell.lib.compose.dontCheck (
-    pkgs.haskell.lib.compose.overrideCabal stageBaikaiRootFiles
-      (doJailbreak (final.callCabal2nix "baikai" (baikai-src + "/baikai") { }))
-  );
-
-  baikai-claude = pkgs.haskell.lib.compose.dontCheck (
-    pkgs.haskell.lib.compose.overrideCabal stageBaikaiRootFiles
-      (doJailbreak (final.callCabal2nix "baikai-claude" (baikai-src + "/baikai-claude") { }))
-  );
-
-  baikai-openai = pkgs.haskell.lib.compose.dontCheck (
-    pkgs.haskell.lib.compose.overrideCabal stageBaikaiRootFiles
-      (doJailbreak (final.callCabal2nix "baikai-openai" (baikai-src + "/baikai-openai") { }))
-  );
-
   seihou-core = pkgs.haskell.lib.compose.overrideCabal
     (drv: {
       # callCabal2nix only copies seihou-core/, so make the schema submodule
